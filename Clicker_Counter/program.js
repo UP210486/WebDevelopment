a81d15a0c8b151ee34fecb9269b0667c959c7d42
Axel Emir Saucedo Zermeño <112653500+UP210486@users.noreply.github.com>
2024-03-05 14:07:29 -0600

    Add files via upload

---
+++ Clicker_Counter/program.js
@@ -0,0 +1,17 @@
+        //Number   
+        let x=1;
+        let y=10;
+        let c=x-y;
+        //string
+        let name= "Axel";
+        let namez ='Saucedo';
+        let namex =`Hola
+        mundo 
+        crue    l   `;
+        //boolean
+        let a=true && true || false;
+        let b=!false;
+        let min_num = 1>=2;
+        //alert(c)
+        let button = document.getElementById("btn");
+        console.log(button);
